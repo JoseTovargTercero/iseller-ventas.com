51c4d11b30f2bf55ebc5930f8e7e63775ac03e56
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2025-03-29 10:35:27 -0400

    `Refactored database configuration in configuracion.php`

--- configurar/configuracion.php
+++ configurar/configuracion.php
@@ -1,47 +1,15 @@
 <?php
-/*
-
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
 	$usuario = 'root';
 	$contrasena = '';
-	$baseDeDatos = 'inventario';
-
-
-	$conexion = new mysqli('localhost', $usuario, $contrasena, $baseDeDatos); 
-	$conexion->set_charset('utf8'); 
-
-	if ($conexion->connect_error) {
-	die('Error de conexion: ' . $conexion->connect_error);
-	}
-
-	date_default_timezone_set('America/Manaus');
-	session_start();
-	error_reporting(0);
-
-
-
-	function contar($condicion){
-		global $conexion;
-
-		//$condicion = "SELECT count(*) FROM $table WHERE $condicion";
-		$stmt = $conexion->prepare($condicion);
-		$stmt->execute();
-		$row = $stmt->get_result()->fetch_row();
-		$galTotal = $row[0];
-
-		return $galTotal;
-	}
-*/
-
-
-/*
+	$baseDeDatos = 'iseller';
+} elseif ($_SERVER['SERVER_NAME'] == 'iseller-tiendas.com') {
 	$usuario = 'userseller';
 	$contrasena = 'B9f(FbTR=sMd';
 	$baseDeDatos = 'iseller';
-*/
+}
 
-$usuario = 'root';
-$contrasena = '';
-$baseDeDatos = 'iseller';
+
 
 
 
