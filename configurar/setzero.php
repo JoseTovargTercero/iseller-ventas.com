e2e78c156d807713dc752ef997e61d392135f083
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2026-05-06 10:59:40 -0400

    feat: add script to reset stock levels for specific products to zero

--- configurar/setzero.php
+++ configurar/setzero.php
@@ -5,35 +5,26 @@
 
     $bss_id = $_SESSION['bss_id'];
 
-    $productos_ignorar = [3803, 4956, 1684, 5440, 1651, ];
+    $productos_ignorar = [3803, 4956, 1684, 5440, 1651, 3804, 4097, 3805, 1657, 5291, 5289,5290, 5397];
 
-    $productos_a_poner_a_cero = [];
 
     //Obtener todos los productos
     $query6 = $conexion->query("SELECT p.id, p.nombre, s.stock FROM productos as p
     LEFT JOIN stock as s ON s.id_producto = p.id
-     WHERE s.bss_id='$bss_id' AND s.id_sucursal='12'");
+     WHERE s.bss_id='$bss_id' AND s.id_sucursal='12' AND p.id NOT IN (
+        3803, 4956, 1684, 5440, 1651, 3804, 4097, 3805, 1657, 5291, 5289,5290, 5397
+     )");
     if ($query6 && $query6->num_rows > 0) {
         while ($row6 = $query6->fetch_assoc()) {
+            $id  = $row6['id'];
+            $id_sucursal = 12;
+
+            $updateStmt = $conexion->prepare("UPDATE stock SET stock = 0 WHERE id = ?");
+            $updateStmt->bind_param("i", $id);
+            $updateStmt->execute();
+            
             echo $row6['id'] . " - " . $row6['nombre'] . " - " . $row6['stock'] . "<br>";
+
         }
     }
 
-
-
-    /*
-Milanesa
-Cochino
-Chuleta ahumada 
-Pollo fresco 
-Pollo entero
-Alas
-Patas
-Molleja 
-Queso llanero
-Jamón fiambre Drago
-Jamón espalda Drago
-Jamón pierna Drago
-Mortadela especial dragó
-
-    */
