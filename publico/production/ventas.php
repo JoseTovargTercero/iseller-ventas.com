6a4fe200b5cb56507ac1f3574ba2c2c4a9383a81
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2026-05-17 20:23:09 -0400

    feat: add sales module interface and inventory retrieval logic in ventas.php

--- publico/production/ventas.php
+++ publico/production/ventas.php
@@ -985,7 +985,7 @@ echo '</pre>';
 
         // Verificar Apertura de caja
         $(document).ready(function() {
-            if(configuraciones.cortes_caja == 1){
+            if(configuraciones.cortes_caja == 1 && nivelUsuario == 2){
                 aperturaCaja()
                 consulaCierre()
             }
